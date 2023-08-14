00466c490db77ec794a0344b06b13a8d92d14659
hegativenn <Hegativenn@yandex.ru>
2023-08-14 18:44:08 +0300

    task2

--- src/main/java/ru/yandex/tasks/Task2SelectionSort.java
+++ src/main/java/ru/yandex/tasks/Task2SelectionSort.java
@@ -10,7 +10,20 @@ public class Task2SelectionSort {
          * Выход: отсортированный (сортировкой выбором!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        for (int i = 0; i < numbers.size(); i++) {
+            int minInd = i;
+            int minElem = numbers.get(i);
+            for (int j = i + 1; j < numbers.size(); j++) {
+                if (minElem > numbers.get(j)) {
+                    minInd = j;
+                    minElem = numbers.get(j);
+                }
+                int tmp = numbers.get(i);
+                numbers.set(i, numbers.get(minInd));
+                numbers.set(minInd, tmp);
+            }
+        }
+        return numbers;
     }
 
     public static void selfCheck() {
