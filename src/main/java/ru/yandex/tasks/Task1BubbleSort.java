c48cfc124881465b2d4befc83937554ec907b60e
hegativenn <Hegativenn@yandex.ru>
2023-08-14 18:34:45 +0300

    task1

--- src/main/java/ru/yandex/tasks/Task1BubbleSort.java
+++ src/main/java/ru/yandex/tasks/Task1BubbleSort.java
@@ -10,7 +10,20 @@ public class Task1BubbleSort {
          * Выход: отсортированный (сортировкой пузырьком!) numbers
          */
         // (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ WRITE CODE HERE (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧
-        return null;
+        boolean stop = false;
+        while (!stop) {
+            stop = true;
+            for (int i = 0; i < numbers.size() - 1; i++) {
+                if (numbers.get(i) > numbers.get(i + 1)) {
+                    stop = false;
+                    int tmp = numbers.get(i);
+                    numbers.set(i, numbers.get(i + 1));
+                    numbers.set(i + 1, tmp);
+                }
+            }
+        }
+
+        return numbers;
     }
 
     public static void selfCheck() {
